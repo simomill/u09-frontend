54bbc2d007b277f7e94adbce6479c802d4f3e270
Simon Miller <mail@simonmiller.se>
2022-10-18 15:48:49 +0200

    make reg&login easier w/ form hooks

--- src/pages/loginPage.tsx
+++ src/pages/loginPage.tsx
@@ -1,46 +1,34 @@
 import React, { ChangeEvent, FC, useEffect, useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 import { login, LoginModel } from "../Services/auth.service";
+import { useForm } from "react-hook-form";
 
 const LoginPage: FC = () => {
     // states and navigation
     const navigate = useNavigate();
-    const [loginData, setLoginData] = useState<LoginModel>({
-        username: "",
-        password: "",
-    });
+    const { state } = useLocation();
     const [loading, setLoading] = useState(false);
-    const [statusMsg, setStatusMsg] = useState("");
+    const [statusMsg, setStatusMsg] = useState(state ?? "");
+
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+    } = useForm();
 
     useEffect(() => {
         if (statusMsg) {
             setTimeout(() => {
                 setStatusMsg("");
-            }, 2000);
+            }, 4000);
         }
     }, [statusMsg]);
 
-    // Change username state from input
-    function handleChangeUsername(e: ChangeEvent<HTMLInputElement>) {
-        setLoginData({
-            username: e.target.value,
-            password: loginData.password,
-        });
-    }
-
-    // Change password state from input
-    function handleChangePassword(e: ChangeEvent<HTMLInputElement>) {
-        setLoginData({
-            username: loginData.username,
-            password: e.target.value,
-        });
-    }
-
     // Login on button click
-    async function handleClickLogin() {
+    async function onSubmit(data: any) {
         setLoading(true);
 
-        const response = await login(loginData);
+        const response = await login(data);
 
         if (
             response.data !== "Wrong Password" &&
@@ -49,7 +37,6 @@ const LoginPage: FC = () => {
             // Navigate and reload if successful
             navigate("/");
             window.location.reload();
-
         } else {
             setStatusMsg(response.data);
         }
@@ -62,36 +49,54 @@ const LoginPage: FC = () => {
         <div className="flex flex-col justify-center items-center h-screen gap-4">
             <h1 className="font-bold text-3xl mb-6 text-cyan-900">DSPLAY</h1>
 
-            <input
-                className="border rounded py-2 px-3 mb-3"
-                type="text"
-                name="username"
-                id="username"
-                placeholder="username"
-                value={loginData.username}
-                onChange={handleChangeUsername}
-            />
-            <input
-                className="border rounded py-2 px-3 mb-3"
-                type="password"
-                name="pass"
-                id="pass"
-                placeholder="password"
-                value={loginData.password}
-                onChange={handleChangePassword}
-            />
-            <input
-                className="border rounded py-2 px-3 bg-slate-50 cursor-pointer"
-                type="submit"
-                value="login"
-                onClick={handleClickLogin}
-            />
+            <form
+                className="flex flex-col"
+                action=""
+                onSubmit={handleSubmit(onSubmit)}
+            >
+                <input
+                    className="border rounded py-2 px-3 mb-3"
+                    type="text"
+                    id="username"
+                    placeholder="username"
+                    {...register("username", { required: true })}
+                />
 
-            <Link to={"/register"} className="text-cyan-900 hover:text-cyan-600">
-                register
-            </Link>
+                <input
+                    className="border rounded py-2 px-3 mb-3"
+                    type="password"
+                    id="pass"
+                    placeholder="password"
+                    {...register("password", { required: true })}
+                />
 
-            {statusMsg && <p className="text-red-700">{statusMsg}</p>}
+                <input
+                    className="border rounded py-2 px-3 bg-slate-50 cursor-pointer"
+                    type="submit"
+                    value="login"
+                />
+
+                <Link
+                    to={"/register"}
+                    className="text-cyan-900 hover:text-cyan-600"
+                >
+                    Not a user yet?
+                </Link>
+
+                {(() => {
+                    if (statusMsg) {
+                        return statusMsg.includes("success") ? (
+                            <p className="text-green-700 bg-green-50 border border-green-700 rounded mt-3">
+                                {statusMsg}
+                            </p>
+                        ) : (
+                            <p className="text-red-700 bg-red-50 border border-red-700 rounded mt-3">
+                                {statusMsg}
+                            </p>
+                        );
+                    }
+                })()}
+            </form>
         </div>
     );
 };
