509a8acc2cda5f19239bb0ce1bfe5df9d28bf502
Simon Miller <mail@simonmiller.se>
2022-11-02 13:28:50 +0100

    change variable name

--- src/Services/auth.service.ts
+++ src/Services/auth.service.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = `${process.env.API_URL}/auth`;
+const API_URL = `${process.env.REACT_APP_API_URL}/auth`;
 
 export interface LoginModel {
     username: string
