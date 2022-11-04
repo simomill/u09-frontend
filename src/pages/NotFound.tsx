f4889c9c2779acc10f7a867599fc3329a472dd69
Simon Miller <mail@simonmiller.se>
2022-11-04 11:08:35 +0100

    add more stuff to 404-page

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from "react";
+import { FaChevronLeft } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
 const NotFound = () => {
-  return (
-    <div>NotFound</div>
-  )
-}
+    return (
+        <div className="flex flex-col items-center justify-center">
+            <h1 className="text-4xl">404</h1>
+            <p>The page you tried to visit does not exist.</p>
 
-export default NotFound
+            <Link
+                className="flex flex-row items-center hover:text-cyan-700"
+                to="/"
+            >
+                <FaChevronLeft />
+                <span>Back</span>
+            </Link>
+        </div>
+    );
+};
+
+export default NotFound;
