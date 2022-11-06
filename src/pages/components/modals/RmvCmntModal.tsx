871838cee408ec2aea1d27fa6f506573d97291fe
Simon Miller <mail@simonmiller.se>
2022-11-06 21:06:48 +0100

    Tailwind Components for RmvCmntModal

---
+++ src/pages/components/modals/RmvCmntModal.tsx
@@ -0,0 +1,63 @@
+import React from "react";
+import { HiOutlineX } from "react-icons/hi";
+import { deleteComment } from "../../../Services/comment.service";
+import { BiCommentX } from "react-icons/bi";
+
+const RemoveCmntModal = ({ showModal, setShowModal, id, fetchComments }: any) => {
+
+    function closeHandler() {
+        setShowModal((prev: any) => !prev);
+    }
+
+    function onApprove() {
+        deleteComment(id).then(() => {
+            fetchComments();
+        });
+        setShowModal((prev: any) => !prev);
+    }
+
+    function onDeny() {
+        setShowModal((prev: any) => !prev);
+    }
+
+    return (
+        <>
+            {showModal && (
+                <div className="commentModalPlacement">
+                    <div className="commentModalBody">
+                        <HiOutlineX
+                            className="closeWndw"
+                            onClick={closeHandler}
+                        />
+
+                        <div className="flex flex-col items-center">
+                            <BiCommentX className="w-8 h-8 mb-3" />
+
+                            <div className="px-8">
+                                <p>You are about to delete this comment.</p>
+                                <p>Are you sure about this?</p>
+                            </div>
+                        </div>
+
+                        <div className="yesNoGroup">
+                            <button
+                                onClick={onApprove}
+                                className="yesNoBtn"
+                            >
+                                Yes
+                            </button>
+                            <button
+                                onClick={onDeny}
+                                className="yesNoBtn"
+                            >
+                                No
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            )}
+        </>
+    );
+};
+
+export default RemoveCmntModal;
