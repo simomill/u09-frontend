01f874e56eb35d3a6075958035d05c46a6057a8a
Simon Miller <mail@simonmiller.se>
2022-11-06 18:52:27 +0100

    tailwind components for LoginPage

--- src/pages/loginPage.tsx
+++ src/pages/loginPage.tsx
@@ -45,8 +45,8 @@ const LoginPage: FC = () => {
 
     return (
         <>
-        <div className="flex flex-col justify-center items-center h-screen gap-4">
-            <h1 className="font-bold text-3xl mb-6 text-cyan-900">DSPLAY</h1>
+        <div className="formGroup">
+            <h1 className="logo">DSPLAY</h1>
 
             <LoginForm state={state} />
 
