d9cb27b4632e554179d072b24a62427f83b39e88
Simon Miller <mail@simonmiller.se>
2022-11-06 20:51:30 +0100

    Tailwind components RmvImgModal

---
+++ src/pages/components/modals/RmvImgModal.tsx
@@ -0,0 +1,56 @@
+import React from "react";
+import { deleteUserPhoto } from "../../../Services/user.service";
+import { HiOutlineX } from "react-icons/hi";
+import { IoWarningOutline } from "react-icons/io5";
+
+const RemoveImgModal = ({ showModal, setShowModal, id }: any) => {
+    function closeHandler() {
+        setShowModal((prev: any) => !prev);
+    }
+
+    function onApprove() {
+        deleteUserPhoto(id);
+        window.location.reload();
+    }
+
+    function onDeny() {
+        setShowModal((prev: any) => !prev);
+    }
+
+    return (
+        <>
+            {showModal && (
+                    <div className="modalQ">
+                        <HiOutlineX
+                            className="closeWndw"
+                            onClick={closeHandler}
+                        />
+
+                        <div className="flex flex-col items-center justify-center">
+                            <IoWarningOutline className="w-6 h-6 mb-3 text-red-700" />
+
+                            <p>You are about to delete this image.</p>
+                            <p>Are you sure about this?</p>
+                        </div>
+
+                        <div className="yesNoGroup">
+                            <button
+                                onClick={onApprove}
+                                className="yesNoBtn"
+                            >
+                                Yes
+                            </button>
+                            <button
+                                onClick={onDeny}
+                                className="yesNoBtn"
+                            >
+                                No
+                            </button>
+                        </div>
+                    </div>
+            )}
+        </>
+    );
+};
+
+export default RemoveImgModal;
